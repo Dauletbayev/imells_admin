b00e3b4078360a7d24d2f4adec9e33058c473d47
Dauletbayev <159714997+Dauletbayev@users.noreply.github.com>
2024-06-19 15:49:07 +0500

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 # Подключение к базе данных
-SQLALCHEMY_DATABASE_URL = 'postgresql://imells_admin:daulet2005@172.16.169.21:5432/imells_users_db'
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///data.db'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://imells_admin:daulet2005@172.16.169.21:5432/imells_users_db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///data.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20, max_overflow=10)
 SessionLocal = sessionmaker(bind=engine)
 
